b28a21fc03e5bcb28a43b2bc3a1bfba76c506ce2
AlekseyFomichenko <128860587+AlekseyFomichenko@users.noreply.github.com>
2024-05-29 11:33:07 +0300

    v0.1

---
+++ Homeworks/FamilyTree/FamilyMember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks.FamiyTree
+{
+    enum Gender { male, female}
+    internal class FamilyMember
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public FamilyMember Mother { get; set; }
+        public FamilyMember Father { get; set; }
+        public FamilyMember[] GetGrandMothers() => [Mother.Mother, Father.Mother];
+        public FamilyMember[] GetGrandFathers() => [Mother.Father, Father.Father];
+        
+        
+    }
+}
