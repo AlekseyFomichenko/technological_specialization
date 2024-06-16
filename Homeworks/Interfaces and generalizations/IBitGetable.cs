e30806867054793a4a021a004a970182c8beeac5
AlekseyFomichenko <128860587+AlekseyFomichenko@users.noreply.github.com>
2024-06-16 14:05:33 +0300

    initial commit

---
+++ Homeworks/Interfaces and generalizations/IBitGetable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks.Interfaces_and_generalizations
+{
+    internal interface IBitGetable
+    {
+        bool GetBitByIndex(byte index);
+        void SetBitByIndex(byte index, bool value);
+    }
+
+}
+
---
+++ Homeworks/Interfaces and generalizations/IControllable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks.Interfaces_and_generalizations
+{
+    internal interface IControllable
+    {
+        bool IsOn {  get; }
+        void On();
+        void Off();
+    }
+}
