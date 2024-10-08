693d54cda0ab4e2c58bbfbc937c14d0cc35c8b87
Алексей Фомиченко <fomichaaf.95@gmail.com>
2024-10-08 20:43:48 +0300

    Task1

--- Homeworks/StreamsAndBuff/Program.cs
+++ Homeworks/StreamsAndBuff/Program.cs
@@ -1,11 +1,33 @@
-﻿namespace Homeworks.StreamAndBuff
+﻿
+
+namespace Homeworks.StreamAndBuff
 {
     internal class Program
     {
         
         static void Main(string[] args)
         {
-            
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            string str = ReaderFrom(args[0]);
+            WriteTo(str, args[1]);
+        }
+
+        private static void WriteTo(string str, string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using StreamWriter writer = new StreamWriter(str);
+                writer.Write(str);
+            }
+        }
+
+        private static string ReaderFrom(string path)
+        {
+            using StreamReader sr = new StreamReader(path);
+            return sr.ReadToEnd();
         }
     }
 }
