70ca12673c9e0eb92fa5ed200d1bce52bc3c1b13
Алексей Фомиченко <fomichaaf.95@gmail.com>
2024-10-30 00:35:37 +0300

    Lesson work. Creating socket

---
+++ Homeworks/Network_application_development/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Homeworks.Network_application_development
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5564);
+                server.Bind(iPEndPoint);
+                server.Blocking = true;
+                Console.WriteLine("Waiting for connecting....");
+                server.Listen(100);
+                Socket socket = server.Accept();
+                Console.WriteLine("Connected!");
+                server.Close();
+            }
+        }
+    }
+}
