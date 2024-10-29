8ffb1234257d74766835e26770666b801a5b7f1e
Алексей Фомиченко <fomichaaf.95@gmail.com>
2024-10-30 02:46:45 +0300

    Lesson work. Add method Disconnect

--- Homeworks/Network_application_development/Program.cs
+++ Homeworks/Network_application_development/Program.cs
@@ -8,14 +8,15 @@ namespace Homeworks.Network_application_development
     {
         static void Main(string[] args)
         {
-            IPAddress[] addresses = Dns.GetHostAddresses("yandex.ru");
-            Console.WriteLine("IP-addresses of site yandex.ru:");
-            foreach (var address in addresses)
-            {
-                Console.WriteLine(address.ToString());
-            }
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(addresses, 80);
+            socket.Connect("yandex.ru", 80);
+            Console.WriteLine($"Connecting to address: {(socket.RemoteEndPoint as IPEndPoint)?.Address}");
+
+            socket.Disconnect(true);
+
+            var task = socket.ConnectAsync("google.ru", 80);
+            task.Wait();
+
             Console.WriteLine($"Connecting to address: {(socket.RemoteEndPoint as IPEndPoint)?.Address}");
         }
     }
