9853c610c3aaf4cfe1d590ab05c2d2fae6274390
Алексей Фомиченко <fomichaaf.95@gmail.com>
2024-10-30 21:23:26 +0300

    Seminar work. Create class Message

---
+++ Homeworks/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    public class Message
+    {
+        public string? Text { get; set; }
+        public DateTime DateTime { get; set; }
+        public string? NicknameFrom { get; set; }
+        public string? NicknameTo { get; set; }
+        public string SerializeMessageToJson() => JsonSerializer.Serialize(this);
+        public static void Deserialize(string message) => JsonSerializer.Deserialize<Message>(message);
+    }
+}
