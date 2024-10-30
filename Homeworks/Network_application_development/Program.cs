e6f6e7e54b145b25a7f619ed186eb0da86c3d1bd
Алексей Фомиченко <fomichaaf.95@gmail.com>
2024-10-30 15:19:09 +0300

    Lesson work. UdpClient

--- Homeworks/Network_application_development/Program.cs
+++ Homeworks/Network_application_development/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -6,24 +7,45 @@ namespace Homeworks.Network_application_development
 {
     internal class Program
     {
+        private static IPEndPoint _ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+
+        static void UpdReceiver()
+        {
+            using (UdpClient server = new UdpClient(12345))
+            {
+                IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6644);
+                int count = 0;
+                while (count < 9000)
+                {
+                    byte[] data = server.Receive(ref localEndPoint);
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        Console.Write(data[i] + " ");
+                    }
+                    count += data.Length;
+                }
+            }
+        }
+        static void UdpSender(byte b)
+        {
+            using (UdpClient client = new UdpClient())
+            {
+                client.Connect(_ep);
+                for (int i = 0; i < 3000; i++)
+                {
+                    client.Send([b]);
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 12345);
-            server.Start();
-
-            using (TcpClient client = server.AcceptTcpClient())
+            new Thread(UpdReceiver).Start();
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine("Connected");
-                StreamReader reader = new StreamReader(client.GetStream());
-                StreamWriter writer = new StreamWriter(client.GetStream());
-
-                string? s = reader.ReadLine();
-                Console.WriteLine(s);
-
-                string r = new string(s?.Reverse().ToArray());
-                writer.WriteLine(r);
-                writer.Flush();
+                byte b = (byte)i;
+                new Thread(() => UdpSender(b)).Start();
             }
+            Console.ReadLine();
         }
     }
 }
