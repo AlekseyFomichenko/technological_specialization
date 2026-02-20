43ce711f3c79d261e4ef5b768bb8b1c41151db33
AlekseyFomichenko <fomichaaf.95@gmail.com>
2026-02-20 16:10:56 +0300

    add payloads

---
+++ Shared/DTO/AckPayload.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTO
+{
+    //payload 0x08
+    internal class AckPayload
+    {
+    }
+}
